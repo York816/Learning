e872a2d2aaa8cf1f19672a657b13b2a611b9a0ba
York.Ye <york816@163.com>
2021-07-19 12:14:24 +0800

    add  info

--- dbbase/pymysql_test.py
+++ dbbase/pymysql_test.py
@@ -6,8 +6,11 @@ db = pymysql.connect(host="192.168.30.98",
                      password="gZxL6mjsru07mRuX",
                      db="crm_v5_api_fund",
                      port=3306)
-
 # 使用 cursor() 方法创建一个游标对象 cursor
 cursor = db.cursor()
+cursor.execute("SHOW DATABASES")  # 输出所有数据库列表：
+for x in cursor:
+    print(x)
+pass
 # 关闭数据库连接
 db.close()
